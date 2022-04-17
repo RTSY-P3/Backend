473aada72f7fbb245272feb81f7d57dc153be072
Yusong Shi <jackshi.sys@gmail.com>
2022-04-16 22:31:18 -0400

    Name update

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
 const AuthRouter = require('./routes/AuthRouter')
-const PostRouter = require('./routes/PostRouter')
+const ProjectRouter = require('./routes/ProjectRouter')
 const app = express()
 
 const PORT = process.env.PORT || 3001
@@ -13,6 +13,6 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 
 app.use('/auth', AuthRouter)
-app.use('/posts', PostRouter)
+app.use('/projects', ProjectRouter)
 
 app.listen(PORT, () => console.log(`Server Running On Port: ${PORT} 🫠`))
