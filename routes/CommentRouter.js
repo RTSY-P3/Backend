967ddf5ce9f05e43925470435c1a19da7488f203
tiffpereira <tiffanympereira@gmail.com>
2022-04-18 13:55:55 -0400

    Get comments controller working now

--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -2,9 +2,8 @@ const router = require('express').Router()
 const controller = require('../controllers/CommentController')
 const middleware = require('../middleware')
 
-router.get('/', controller.GetComment)
+router.get('/', controller.GetComments)
 router.post('/', middleware.stripToken, middleware.verifyToken, controller.CreateComment)
-router.put('/:comment_id', middleware.stripToken, middleware.verifyToken, controller.UpdateComment)
 router.delete('/:comment_id', middleware.stripToken, middleware.verifyToken, controller.DeleteComment)
 
 module.exports = router
