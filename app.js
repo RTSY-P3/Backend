66f4da3319d3b9cf1f49826cb1122fa8ac9640fd
tiffpereira <tiffanympereira@gmail.com>
2022-04-19 09:29:36 -0400

    added code for deployment

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const AuthRouter = require('./routes/AuthRouter')
 const ProjectRouter = require('./routes/ProjectRouter')
 const CommentRouter = require('./routes/CommentRouter')
 const app = express()
+const path = require('path')
 
 const PORT = process.env.PORT || 3001
 
@@ -16,5 +17,12 @@ app.use(bodyParser.json())
 app.use('/auth', AuthRouter)
 app.use('/projects', ProjectRouter)
 app.use('/projects/comments', CommentRouter)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')))
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+    })
+  }
 
 app.listen(PORT, () => console.log(`Server Running On Port: ${PORT} 🫠`))
