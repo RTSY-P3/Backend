a49a7e540963e98a4250d04f9b4ecd68054fdd41
ryan.ditzel <ryand370@gmail.com>
2022-04-19 10:10:00 -0400

    password changed to passwordDigest in seed

--- seeders/20220416192829-user.js
+++ seeders/20220416192829-user.js
@@ -6,105 +6,105 @@ module.exports = {
         {
           name: 'John Wick',
           email: 'john@codr.com',
-          password: '$321!pass!123$',
+          passwordDigest: '$321!pass!123$',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'Fujii Jacek',
           email: 'fujike@codr.com',
-          password: '$ss!123$',
+          passwordDigest: '$ss!123$',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'Dai Akira',
           email: 'dai@codr.com',
-          password: '$321!pass!123$',
+          passwordDigest: '$321!pass!123$',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'Hussein Alicia',
           email: 'alicia@codr.com',
-          password: '$321!pass!123$',
+          passwordDigest: '$321!pass!123$',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'Ray Allen',
           email: 'rallen@codr.com',
-          password: '$321!pass!123$',
+          passwordDigest: '$321!pass!123$',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'Kobe Bryant',
           email: 'kbryant@codr.com',
-          password: '$321!pass!123$',
+          passwordDigest: '$321!pass!123$',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'Isa Garba',
           email: 'isag@codr.com',
-          password: '$321!pass!123$',
+          passwordDigest: '$321!pass!123$',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'Feng Bongani',
           email: 'bogani@codr.com',
-          password: '$321!pass!123$',
+          passwordDigest: '$321!pass!123$',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'Udo Noel',
           email: 'nelu@codr.com',
-          password: '$321!pass!123$',
+          passwordDigest: '$321!pass!123$',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'Soto Mphon',
           email: 'soto@codr.com',
-          password: '$321!pass!123$',
+          passwordDigest: '$321!pass!123$',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'Rabiu Jesus',
           email: 'rabiu@codr.com',
-          password: '$321!pass!123$',
+          passwordDigest: '$321!pass!123$',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'Kanye West',
           email: 'kwest@codr.com',
-          password: '$321!pass!123$',
+          passwordDigest: '$321!pass!123$',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'J Cole',
           email: 'jcole@codr.com',
-          password: '$321!pass!123$',
+          passwordDigest: '$321!pass!123$',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'John Wick',
           email: 'john@codr.com',
-          password: '$321!pass!123$',
+          passwordDigest: '$321!pass!123$',
           createdAt: new Date(),
           updatedAt: new Date()
         },
         {
           name: 'John Wick',
           email: 'john@codr.com',
-          password: '$321!pass!123$',
+          passwordDigest: '$321!pass!123$',
           createdAt: new Date(),
           updatedAt: new Date()
         }
