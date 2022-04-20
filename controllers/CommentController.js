d997dbe0bd7c14f146c4f78183f103c52c4e1580
Yusong Shi <jackshi.sys@gmail.com>
2022-04-20 12:37:09 -0400

    huh?

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -1,18 +1,11 @@
-<<<<<<< .merge_file_ntjosm
+
 const { Comments } = require('../models')
+const { Post } = require('../models')
 
 const GetComments = async (req, res) => {
   try {
     const comments = await Comments.findAll()
     res.send(comments)
-=======
-const { Post } = require('../models')
-
-const GetComments = async (req, res) => {
-  try {
-    const posts = await Post.findAll()
-    res.send(posts)
->>>>>>> .merge_file_WVUMH8
   } catch (error) {
     throw error
   }
@@ -20,23 +13,18 @@ const GetComments = async (req, res) => {
 
 const CreateComment = async (req, res) => {
   try {
-<<<<<<< .merge_file_ntjosm
+
     const comment = await Comments.create({ ...req.body })
-=======
-    const comment = await Comment.create({ ...req.body })
->>>>>>> .merge_file_WVUMH8
+
     res.send(comment)
   } catch (error) {
     throw error
   }
 }
 
-<<<<<<< .merge_file_ntjosm
 
-const DeleteComment = async (req, res) => {
-  try {
-    await Comments.destroy({ where: { id: req.params.comment_id } })
-=======
+
+
 const UpdateComment = async (req, res) => {
   try {
     const comment = await Comment.update(
@@ -52,7 +40,6 @@ const UpdateComment = async (req, res) => {
 const DeleteComment = async (req, res) => {
   try {
     await Comment.destroy({ where: { id: req.params.comment_id } })
->>>>>>> .merge_file_WVUMH8
     res.send({ msg: 'Comment Deleted', payload: req.params.comment_id, status: 'Ok' })
   } catch (error) {
     throw error
@@ -62,9 +49,6 @@ const DeleteComment = async (req, res) => {
 module.exports = {
   GetComments,
   CreateComment,
-<<<<<<< .merge_file_ntjosm
-=======
   UpdateComment,
->>>>>>> .merge_file_WVUMH8
   DeleteComment
 }
